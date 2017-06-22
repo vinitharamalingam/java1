cd9bfaf74c5b19e680f877420834a024e4fa8ffe
vinitharamalingam <vinitharam1996@gmail.com>
2017-06-22 20:58:59 +0530

    Create Test_2.java

---
+++ Test_2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Test_2{
+public static void main(String []args)
+{
+int x;
+Scanner in=new Scanner(System.in);
+x=in.nextInt();
+if(x > 0)
+System.out.println("X is positive");
+else if(x==0)
+System.out.println("X is Zero");
+else
+System.out.println("X is negative");
+}
+}
